1e7880308f4bde18e60f10ef366729ca7d0052f6
Zapizza India <zapizzaindia@gmail.com>
2026-02-04 15:14:39 +0000

    i have png . i didnt understand where did you say to update it exactly?

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -4,14 +4,40 @@ import Image from 'next/image';
 /**
  * ZapizzaLogo Component
  * 
- * To use your own custom logo:
- * 1. SVG: Replace the <path> elements inside the <svg> tag below with your own SVG paths.
- * 2. IMAGE (PNG/JPG): 
- *    - Place your logo file (e.g., logo.png) in the /public directory.
- *    - Uncomment the "Image version" code block and comment out the "SVG version".
+ * HOW TO USE YOUR CUSTOM LOGO:
+ * 1. Upload your image file (e.g., logo.png) to the "public" folder.
+ * 2. Ensure the filename matches what is used in the <img> tag below.
  */
 export function ZapizzaLogo(props: SVGProps<SVGSVGElement>) {
-  // --- SVG VERSION (Current) ---
+  // --- IMAGE VERSION (Active) ---
+  // This uses a PNG file from your /public folder.
+  // To use your own, upload a file named "logo.png" to the public folder.
+  return (
+    <div 
+      className={props.className} 
+      style={{ 
+        width: props.width || '100%', 
+        height: props.height || '100%', 
+        position: 'relative',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center'
+      }}
+    >
+      <img 
+        src="/logo.png" 
+        alt="Zapizza" 
+        style={{ 
+          maxWidth: '100%', 
+          maxHeight: '100%', 
+          objectFit: 'contain' 
+        }} 
+      />
+    </div>
+  );
+
+  /*
+  // --- SVG VERSION (Commented Out) ---
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -25,24 +51,11 @@ export function ZapizzaLogo(props: SVGProps<SVGSVGElement>) {
       strokeLinejoin="round"
       {...props}
     >
-      {/* Replace these paths with your custom SVG paths */}
       <path d="M12 2a10 10 0 0 0-9.4 13.5c.6.8 1.5 1.2 2.4 1.2H20a2 2 0 0 0 1.9-2.8A10 10 0 0 0 12 2Z" />
       <path d="M12 12V2" />
       <path d="M15 13a3 3 0 1 0-6 0" />
       <path d="M13.5 10.5a1.5 1.5 0 1 0-3 0" />
     </svg>
-  );
-
-  /*
-  // --- IMAGE VERSION (Uncomment to use a PNG/JPG from /public) ---
-  return (
-    <div className={props.className} style={{ width: props.width || '100%', height: props.height || '100%', position: 'relative' }}>
-      <img 
-        src="/logo.png" 
-        alt="Custom Logo" 
-        style={{ width: '100%', height: '100%', objectFit: 'contain' }} 
-      />
-    </div>
   );
   */
 }
