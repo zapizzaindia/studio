fc3ed6eba500a087846c8a3a5887d364407a7cce
Zapizza India <zapizzaindia@gmail.com>
2026-02-18 16:33:52 +0000

    commit

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDIkI00RI3nfKPkcR33rDyqs_12TK4jw2M",
+  apiKey: "AIzaSyDIkIO0RI3nfKPkcR33rDyqs_12TK4jw2M",
   authDomain: "zapizza-backend.firebaseapp.com",
   projectId: "zapizza-backend",
   storageBucket: "zapizza-backend.firebasestorage.app",
   messagingSenderId: "197006828213",
-  appId: "1:197006828213:web:26179eeb89f86ce6b94fc",
+  appId: "1:197006828213:web:261179eeb89f86ce6b94fc",
   measurementId: "G-V1TMNB954N"
 };
 
