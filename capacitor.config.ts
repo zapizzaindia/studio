bd4b2f0cb7ec14651e63859b049a643aaea6f7ac
Zapizza India <zapizzaindia@gmail.com>
2026-03-19 11:14:15 +0000

    commit

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,10 +4,7 @@ const config: CapacitorConfig = {
   appId: 'com.zapizza.pos',
   appName: 'Zapizza POS',
   webDir: 'out',
-  server: {
-    url: "https://orders.zapizza.co.in/",
-    cleartext: false
-  }
+  
 };
 
 export default config;
--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,10 @@
-import type {NextConfig} from 'next';
+import type { NextConfig } from 'next';
 import withPWAInit from "@ducanh2912/next-pwa";
 
 const withPWA = withPWAInit({
   dest: "public",
   register: true,
   disable: process.env.NODE_ENV === "development",
-  // Aggressive caching for images to ensure "Instant" feel
   cacheOnFrontEndNav: true,
   aggressiveFrontEndNavCaching: true,
   reloadOnOnline: true,
@@ -13,7 +12,8 @@ const withPWA = withPWAInit({
 });
 
 const nextConfig: NextConfig = {
-  /* Core configuration options */
+  output: 'export', // 🔥 ADD THIS LINE
+
   typescript: {
     ignoreBuildErrors: true,
   },
@@ -25,26 +25,18 @@ const nextConfig: NextConfig = {
       {
         protocol: 'https',
         hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: 'firebasestorage.googleapis.com',
-        port: '',
-        pathname: '/**',
       },
     ],
   },
