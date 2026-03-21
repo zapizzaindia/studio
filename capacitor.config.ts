33cd942cd6b1d23effe71217caf3ac238c601c12
Zapizza India <zapizzaindia@gmail.com>
2026-03-21 16:38:18 +0000

    c

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,8 +2,12 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.zapizza.pos',
-  appName: 'Zapizza',
+  appName: 'Zapizza Partners',
   webDir: 'out'
+server: {
+  url: "https://orders.zapizza.co.in/admin/login",
+  cleartext: false
+}
 };
 
 export default config;
